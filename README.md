4660e7d07b80493731ef93bb6b4e03e24496cf39
LittleVigney <1053813361@qq.com>
2025-05-06 22:00:31 +0800

    client.py: comments

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-This is my assignment 3 for COMPX234.
+### This is my assignment 3 for COMPX234.
 
-Comments:
-server.py
+#### server.py
+
+##### TupleSpaceServer: server class
+
+- **init**: constructor
+
+  - ts_data: dictionary store all tuples of the server.
+
+  - server_port: port number for server.
+
+  - ts_state: state number for server
+
+- **update_states**: update state of server according to the operation executed.
+- **read**: read action.
+- **get**: get action.
+- **put**: put action.
+- **cal_info**: calculate and update the state.
+- **display_info**: print the server state.
+
+##### handle_client: method for server to handle one client
+
+##### start_server: start server and waiting for clients connection.
+
+
+
+#### client.py
+
+##### TupleSpaceClient: client class
+
+- **init**: constructor
+  - **request_data**: data clients will send to server
+  - **origin_data**: original data clients loads from files.
+  - **port**: server port
+  - **filename**: file name which the client will load
+  - **socket_addr**: address of server socket.
+
+- **read_data**: load data from file and change original data to the protocol format.
+
+##### start_client: start one client and send its requests to server.
--- client.py
+++ client.py
@@ -10,15 +10,18 @@ class TupleSpaceClient:
         self.socket_addr = ("localhost", self.port)
 
     def read_data(self):
+        # open file and load data
         with open(self.filename, 'r') as f:
             while True:
                 line = f.readline()
 
+                # if loading finish
                 if not line:
                     break
                 
                 self.origin_data.append(line)
-
+                
+                # get k and v from original data
                 v = ""
                 lines = line.split()
 
@@ -31,21 +34,24 @@ class TupleSpaceClient:
                     k = lines[1]
                     vbegin = len(k) + len("PUT") + 2
                     v = line[vbegin : ]
-            
+
+                # calculate size of request
                 size_num = 7
                 size_num += len(k)
                 if op == "PUT":
                     size_num += 1 + len(v)
                 size = f"{size_num:03d}"
-            
-                # 如果请求长度大于999
 
+                # add request in form of protocol to request_data
                 rq_info = size + " " + op[0] + " " + k
                 if op == "PUT":
                     rq_info += " " + v
-            
+
+                # if k and v is longer than 970
+                if len(k) + len(v) + 1 > 999:
+                    rq_info = "LONG REQUEST"
+
                 self.request_data.append(rq_info)
-                # print(rq_info)
         
 def start_client(_filename, _port):
         my_client = TupleSpaceClient(_filename, _port)
@@ -58,10 +64,18 @@ def start_client(_filename, _port):
 
         ct = 0
         for every_rq in my_client.request_data:
+            # if k and v is longer than 970, output an error message
+            if every_rq == "LONG REQUEST":
+                print("Request is too long.")
+                continue
+            
+            # send request to server
             client_socket.sendall(every_rq.encode('utf-8'))
 
+            # get response from server
             res = client_socket.recv(4096)
 
+            # print result of request
             print(my_client.origin_data[ct] + " " + res.decode('utf-8'))
 
             ct += 1
